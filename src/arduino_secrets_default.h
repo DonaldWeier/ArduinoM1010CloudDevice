57c5769894739497845f1f159768b91a014ba9fd
Don Weier <deweier.bme@gmail.com>
2023-06-18 17:00:13 -0500

    removed references form example that relied on incorrect service. Included notes for adding secret infor to project.

---
+++ src/arduino_secrets_default.h
@@ -0,0 +1,15 @@
+
+#pragma once
+#include <string>
+
+//examples of the minimum required set of definitions below. The project includes 'arduino_secrets.h' but it is exluded 
+// from version control for reasons that are hopefully obvious.
+
+#define SECRET_WIFI_SSID        "placeholder"
+#define SECRET_WIFI_PASS        "password"
+
+#define SECRET_BROKER           "placeholder.azure-devices.net"
+
+#define SECRET_DEVICE_ID        "placeholderDeviceID"
+
+#define SECRET_DEVICE_PASSWORD  "placeholder again"
